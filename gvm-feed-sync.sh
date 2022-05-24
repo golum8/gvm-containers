236762afb9f51c2dc37391f1720357d2d58c8e28
Kubiak <artur.kubiak@orange.com>
2022-05-24 15:56:12 +0200

    Dostosowanie do aktualnej wersji

--- gvm-feed-sync.sh
+++ gvm-feed-sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker run -d --rm --name tmp_container -v gvm-containers_gvm-var-lib:/opt alpine tail -f /dev/null
+docker exec tmp_container sh -c 'rm -rf /opt/cert-data/* /opt/data-objects/gvmd/* /opt/gvmd/report_formats/*  /opt/scap-data/*'
 docker cp /opt/gvm-data/gvm/cert-data  tmp_container:/opt/
 docker cp /opt/gvm-data/gvm/data-objects  tmp_container:/opt/
 docker cp /opt/gvm-data/gvm/gvmd  tmp_container:/opt/
@@ -8,4 +9,3 @@ docker cp /opt/gvm-data/gvm/scap-data  tmp_container:/opt/
 docker exec -it tmp_container /bin/chown -R 102:103 /opt/
 
 docker stop tmp_container
-
