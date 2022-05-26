732022040693474effd93e640c798afaade5a883
Kubiak <artur.kubiak@orange.com>
2022-05-26 13:17:01 +0200

    Poprawka

--- admin/mixeway-pki-copy.sh
+++ admin/mixeway-pki-copy.sh
@@ -4,6 +4,6 @@
 # Java nie moe być uruchamiana z uytkownika root dlatego trzba zmienić ownera plików 
 
 docker run -d --rm --name tmp_container -v gvm-containers_mixeway-pki:/opt alpine tail -f /dev/null
-docker cp ~/mixeway/pki tmp_container:/opt/
+docker cp ~/mixeway/pki/. tmp_container:/opt/
 docker exec -it tmp_container /bin/chown -R 102:103 /opt/
 docker stop tmp_container
