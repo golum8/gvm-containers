768c1914676073a09adda90a93583fa22618c8e0
Kubiak <artur.kubiak@orange.com>
2022-05-26 12:22:43 +0200

    mixeway: zastąpienie mapowania lokalnego systemu plików do dockera przez volumin Przeniesienie skryptów do katalogu admin

---
+++ admin/mixeway-pki-copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Kopiowanie plików mixeway.p12 i trust.jks do voluminu dokera. 
+# Java nie moe być uruchamiana z uytkownika root dlatego trzba zmienić ownera plików 
+
+docker run -d --rm --name tmp_container -v gvm-containers_mixeway-pki:/opt alpine tail -f /dev/null
+docker cp ~/mixeway/pki tmp_container:/opt/
+docker exec -it tmp_container /bin/chown -R 102:103 /opt/
+docker stop tmp_container
